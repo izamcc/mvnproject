9adc00d91cd1f7d2d162fd299a711d960f231551
izamcc <izamcc5@gmail.com>
2022-05-09 14:22:32 +0300

    all new project files

---
+++ src/main/java/com/mvnproject1/mvnproject1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mvnproject1.mvnproject1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Mvnproject1Application.class);
+	}
+
+}
---
+++ src/test/java/com/mvnproject1/mvnproject1/Mvnproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvnproject1.mvnproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvnproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
