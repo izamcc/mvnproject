91d9806d0c0a4a63add7606b135cd05a53ba9b01
izamcc <izamcc5@gmail.com>
2022-05-10 09:42:26 +0300

    mvnproject1

---
+++ src/main/java/com/mvnproject1/mvnproject1/Mvnproject1Application.java
@@ -0,0 +1,17 @@
+package com.mvnproject1.mvnproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Mvnproject1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Mvnproject1Application.class, args);	}
+
+	@GetMapping
+	public String hello() { return "Hello There !!!";}
+
+}
