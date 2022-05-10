79d960beae543d08ed598e477fbd9b9ab97c1e4e
izamcc <izamcc5@gmail.com>
2022-05-10 16:13:52 +0300

    edit code fix error

--- src/main/java/com/mvnproject1/mvnproject1/Mvnproject1Application.java
+++ src/main/java/com/mvnproject1/mvnproject1/Mvnproject1Application.java
@@ -1,4 +1,4 @@
-package com.mvnproject.mvnproject;
+package com.mvnproject1.mvnproject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
